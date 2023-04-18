273926f826dc579158ea3c4c5856a4514f9c6a62
Kapusta-fairy <dknenorm63@gmail.com>
2023-04-18 18:32:24 +0300

    add dice

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+from random import randint
+
 from kivy.config import Config
 Config.set('graphics', 'width', '420')
 Config.set('graphics', 'height', '260')
 Config.set('graphics', 'minimum_width', '420')
 Config.set('graphics', 'minimum_height', '260')
-
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 
 
 class Dice(object):
-    ...
+
+    def __init__(self, sides):
+        self.sides = sides
+
+    def throw(self):
+        return randint(1, self.sides)
 
 
 class Container(MDBoxLayout):
