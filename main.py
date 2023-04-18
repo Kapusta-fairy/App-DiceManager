da6784957347e7c1882c1aa75bad4e05fa23b0e5
Kapusta-fairy <dknenorm63@gmail.com>
2023-04-18 18:03:55 +0300

    set window config

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from kivy.config import Config
+Config.set('graphics', 'width', '420')
+Config.set('graphics', 'height', '260')
+Config.set('graphics', 'minimum_width', '420')
+Config.set('graphics', 'minimum_height', '260')
+
 from kivymd.app import MDApp
 from kivymd.uix.boxlayout import MDBoxLayout
 
@@ -11,7 +17,7 @@ class Container(MDBoxLayout):
 
 
 class MainApp(MDApp):
-    title = 'Калькулятор дайсов'
+    title = 'Калькулятор ранений'
 
     def build(self):
         return Container()
