3582e70e7dc0bf5949c834d80f7774ae4120041d
Kapusta-fairy <dknenorm63@gmail.com>
2023-04-18 21:00:39 +0300

    restruct main

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
-from random import randint
-
 from kivy.config import Config
 Config.set('graphics', 'width', '420')
 Config.set('graphics', 'height', '260')
 Config.set('graphics', 'minimum_width', '420')
 Config.set('graphics', 'minimum_height', '260')
+
+from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
 
-
-class Dice(object):
-
-    def __init__(self, sides: int):
-        self.sides: int = sides
-
-    def throw(self) -> int:
-        return randint(1, self.sides)
-
-
-class Container(MDBoxLayout):
-    ...
+from mvc.controller import Container
 
 
 class MainApp(MDApp):
     title = 'Калькулятор ранений'
+
+    def __init__(self):
+        super().__init__()
+        Builder.load_file('mvc/view.kv')
 
     def build(self):
         return Container()
