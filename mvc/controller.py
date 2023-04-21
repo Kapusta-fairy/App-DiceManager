14a8cfa3a7db4e296d02096a30b9b1e31fe24d49
Kapusta-fairy <dknenorm63@gmail.com>
2023-04-21 09:19:59 +0300

    change name dice_panel

--- mvc/controller.py
+++ mvc/controller.py
@@ -6,24 +6,27 @@ from mvc.model import Dice, Wound
 class DicePanel(MDBoxLayout):
 
     def remove(self) -> bool:
-        Container.dices.remove(self)
+        Container().remove_dice_panel(self)
         self.parent.remove_widget(self)
 
         return True
 
 
 class Container(MDBoxLayout):
-    dices = list()
+    _dice_panels = list()
 
     def __init__(self):
         super().__init__()
         self.wound = None
 
+    def remove_dice_panel(self, dice_panel: DicePanel):
+        return self._dice_panels.remove(dice_panel)
+
     def view_throws(self) -> str:
         self.wound = Wound()
         dices_result: list = []
 
-        for dice_panel in self.dices:
+        for dice_panel in self._dice_panels:
             sides: str = dice_panel.ids.sides_input.text
             count: str = dice_panel.ids.count_input.text
             if sides not in ['', '0'] and count not in ['', '0']:
@@ -34,9 +37,9 @@ class Container(MDBoxLayout):
         self.result.text = f'{self.wound}'
         return f'{self.result.text}'
 
-    def add_dice(self) -> Dice:
-        new_dice = DicePanel()
-        self.dices.append(new_dice)
-        self.add_widget(new_dice, index=3)
+    def add_dice_panel(self) -> Dice:
+        new_dice_panel = DicePanel()
+        self._dice_panels.append(new_dice_panel)
+        self.add_widget(new_dice_panel, index=3)
 
-        return new_dice
+        return new_dice_panel
