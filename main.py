5719af142d88bb7f321987c8e1d24e8b5e1eb80c
Kapusta-fairy <dknenorm63@gmail.com>
2023-04-21 09:31:07 +0300

    optimization config

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from kivy.config import Config
-Config.set('graphics', 'width', '420')
-Config.set('graphics', 'height', '360')
-Config.set('graphics', 'minimum_width', '420')
-Config.set('graphics', 'minimum_height', '260')
-
-from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
 
 from mvc.controller import Container
 
@@ -16,6 +11,9 @@ class MainApp(MDApp):
     def __init__(self):
         super().__init__()
         Builder.load_file('mvc/view.kv')
+        Window.size = (420, 360)
+        Window.minimum_width = 420
+        Window.minimum_height = 260
 
     def build(self):
         return Container()
